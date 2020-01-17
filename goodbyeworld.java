badfa9af9bd255bd75391ebdd25d85d2e931da32
Jude Wakim <59939123+Judewakim@users.noreply.github.com>
2020-01-16 19:02:25 -0500

    Assignment 1
    hello world and goodbye world program

---
+++ goodbyeworld.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class goodbyeworld {
+	public static void goodbyeworld() {
+		// TODO Auto-generated method stub
+		System.out.println("Goodbye, World!");
+	}
+
+}
---
+++ helloworld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+		
+		goodbyeworld goodbyeObject = new goodbyeworld();
+		goodbyeObject.goodbyeworld();
+		
+	}
+
+}
+
